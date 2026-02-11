92cfdf929652a9e78102b685792945588db9564f
Kennedy <kennedyxjesse@gmail.com>
2026-02-10 21:17:14 -0400

    fixes broken sound and notification button

--- renderer.js
+++ renderer.js
@@ -5,6 +5,30 @@ const REFERENCE_RESET = new Date("2026-01-01T00:30:00-04:00").getTime();
 
 const countdownEl = document.getElementById("countdown");
 const nextResetEl = document.getElementById("nextReset");
+const bellToggle = document.getElementById("bellToggle");
+const alertSound = document.getElementById("alertSound");
+const soundTestBtn = document.getElementById("soundTest");
+
+if (Notification.permission !== "granted") {
+  Notification.requestPermission();
+}
+
+let notificationsEnabled = true;
+let notified5 = false;
+let notified1 = false;
+
+// toggle notifications
+bellToggle.addEventListener("click", () => {
+  notificationsEnabled = !notificationsEnabled;
+  bellToggle.classList.toggle("on", notificationsEnabled);
+});
+
+// testings
+soundTestBtn.addEventListener("click", () => {
+  alertSound.currentTime = 0;
+  alertSound.play();
+});
+
 
 function updateTimer() {
   const now = Date.now();
@@ -21,6 +45,7 @@ function updateTimer() {
   nextResetEl.innerText = "Next reset at: " + nextReset.toLocaleTimeString();
 
   updateColors(timeLeft);
+  checkNotifications(timeLeft);
 
 
   // sending time to main for tray tooltip
@@ -39,7 +64,34 @@ function updateColors(timeLeft) {
   }
 }
 
+function sendAlert(message) {
+  new Notification("Feebas Alert", { body: message });
+
+  alertSound.currentTime = 0;
+  alertSound.play();
+}
+
+function checkNotifications(timeLeft) {
+  if (!notificationsEnabled) return;
+
+  if (timeLeft <= 5 * 60 * 1000 && !notified5) {
+    sendAlert("Tile resets in 5 minutes!");
+    notified5 = true;
+  }
+
+  if (timeLeft <= 60 * 1000 && !notified1) {
+    sendAlert("Tile resets in 1 minute!");
+    notified1 = true;
+  }
+
+  if (timeLeft > 5 * 60 * 1000) {
+    notified5 = false;
+    notified1 = false;
+  }
+}
+
 setInterval(updateTimer, 1000);
 updateTimer();
+
 
 
